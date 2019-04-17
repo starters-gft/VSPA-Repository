1359ef5f42462391705727fcba0bd8d8e9e43ca6
ViniciusLeme <Vinicius.Pierina@gft.com>
2019-04-17 09:45:40 -0300

    first commit

---
+++ alurabank/app/js/models/Negociacao.js
@@ -0,0 +1,25 @@
+// app/js/models/Negociacao.js
+class Negociacao {
+    //Construtor
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    //Get data
+    get data() {
+        return this._data;
+    }
+    //Get quantidade
+    get quantidade() {
+        return this._quantidade;
+    }
+    //Get valor
+    get valor() {
+        return this._valor;
+    }
+    //Get volume -> Quantidade * Valor
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
